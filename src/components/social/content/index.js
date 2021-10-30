7393a0af6fdf68ac9fdeb2b863907ececbc36fd8
António Carlos sousa <1170300@isep.ipp.pt>
2021-10-30 14:29:56 +0100

    remove facebook from social list

--- src/components/social/content/index.js
+++ src/components/social/content/index.js
@@ -8,10 +8,6 @@ const links = [
     url: "https://www.linkedin.com/in/ant%C3%B3nio-sousa-55378a133/",
   },
   {
-    label: "Facebook",
-    url: "https://www.facebook.com/Sr.AntonioSousa",
-  },
-  {
     label: "Instagram",
     url: "https://www.instagram.com/antonio.carlos.sousa/",
   },
